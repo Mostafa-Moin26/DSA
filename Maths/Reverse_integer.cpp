95e3ea3e03de8695c1d3d39bb73b9aef0c03e612
Mostafa Al Moin <sa479374@gmail.com>
2024-08-01 23:16:25 +0600

    solution

---
+++ Maths/Reverse_integer.cpp
@@ -0,0 +1,23 @@
+// Problem Link ---->
+https://leetcode.com/problems/reverse-integer/description/
+
+
+// Solution ------>
+class Solution {
+public:
+    int reverse(int x) {
+        
+        int ans = 0;
+
+        while (x!=0)
+        {
+            int digit = x%10;
+
+            if ((ans>INT_MAX/10) || (ans<INT_MIN/10))
+            return 0;
+            ans = ans * 10 + digit;
+            x/=10;
+        }
+        return ans;
+    }
+};
