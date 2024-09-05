b57fc4902677941a0b0d8452a918647f5e839b23
Mostafa Al Moin <sa479374@gmail.com>
2024-09-05 19:21:27 +0600

    solution

---
+++ Sorting/Selection sort.cpp
@@ -0,0 +1,75 @@
+// Problem Link -----> 
+https://www.geeksforgeeks.org/problems/selection-sort/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=selection-sort
+
+
+// solution ----->
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    int select(int arr[], int i)
+    {
+        // code here such that selectionSort() sorts arr[]
+        selectionSort(arr, i);
+    }
+     
+    void selectionSort(int arr[], int n)
+    {
+       //code here
+       for (int i = 0; i < n; ++i) {
+           int mx = -1, idx;
+           for (int j = 0; j < n - i; ++j) {
+               if (arr[j] > mx) {
+                   mx = arr[j];
+                   idx = j;
+               }
+           }
+           swap(arr[idx], arr[n - i - 1]);
+       }
+    }
+};
+
+//{ Driver Code Starts.
+ 
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    int arr[1000],n,T,i;
+  
+    scanf("%d",&T);
+    
+    while(T--){
+        
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+      
+    Solution ob;  
+    ob.selectionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
