3f2233ef4d10e295e18c3d36ff2cd618d2f0e836
Mostafa Al Moin <sa479374@gmail.com>
2024-09-06 09:01:30 +0600

    little bit optimization

--- Sorting/Bubble Sort.cpp
+++ Sorting/Bubble Sort.cpp
@@ -26,12 +26,16 @@ class Solution {
     // Function to sort the array using bubble sort algorithm.
     void bubbleSort(int arr[], int n) {
         // Your code here
-        
+        bool didSwap = false;
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n - i - 1; ++j) {
                 if (arr[j] > arr[j + 1]) {
                     swap(arr[j], arr[j + 1]);
+                    didSwap = true;
                 }
+            }
+            if (!didSwap) {
+                break;
             }
         }
     }
