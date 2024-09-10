26786556915033e457cd7e2a1559827e3e0cdd32
Mostafa Al Moin <sa479374@gmail.com>
2024-09-10 09:31:14 +0600

    optimization

--- Array/Easy/Check If Array is Sorted and Rotated.cpp
+++ Array/Easy/Check If Array is Sorted and Rotated.cpp
@@ -24,3 +24,24 @@ public:
         
     }
 };
+
+// optimization (Beats 100%)
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+
+        int cnt = 0;
+
+        for (int i = 0; i < nums.size() - 1; ++i) {
+
+            if (nums[i] > nums[i + 1]) {
+                cnt++;
+            }
+        }
+
+        if (nums[0] < nums[nums.size() - 1]) cnt++;
+
+        return cnt <= 1;
+        
+    }
+};
