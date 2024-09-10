d23a5e213770265b0857623651b3960964a86dff
Mostafa Al Moin <sa479374@gmail.com>
2024-09-10 20:48:37 +0600

    solution

--- Array/Easy/Rotate Array Right by k places.cpp
+++ Array/Easy/Rotate Array Right by k places.cpp
@@ -24,3 +24,17 @@ public:
 
     }
 };
+
+// Optimal
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        k = k % n;
+
+        reverse(nums.begin(), nums.begin() + (n - k));
+        reverse(nums.begin() + (n - k), nums.begin() + n);
+        reverse(nums.begin(), nums.end());
+    }
+};
