de286c4bd922b33a6c9c5606e19a300f277b59bf
Mostafa Al Moin <sa479374@gmail.com>
2024-09-13 08:16:10 +0600

    solution

--- Array/Easy/Single Number.cpp
+++ Array/Easy/Single Number.cpp
@@ -4,7 +4,56 @@ https://leetcode.com/problems/single-number/description/
 
 // Solutions ----->
 
-// using xor
+// brute force
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+
+        int ans;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int cnt = 0;
+
+            for (int j = 0; j < nums.size(); j++) {
+                if (nums[i] == nums[j]) {
+                    cnt++;
+                }
+            }
+            if (cnt == 1) {
+                ans = nums[i];
+                break;
+            }
+        }
+
+        return ans;
+        
+    }
+};
+
+// better
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+
+        map<int, int> mp;
+        int ans;
+        for (int i = 0; i < nums.size(); i++) {
+            mp[nums[i]]++;
+        }
+
+        for (auto i : mp) {
+            if (i.second == 1) {
+                ans = i.first;
+                break;
+            }
+        }
+
+        return ans;
+        
+    }
+};
+
+// using xor (optimal)
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
