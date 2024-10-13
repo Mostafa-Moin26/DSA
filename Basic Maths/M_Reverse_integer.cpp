e96a47f4e2b6322fdbf2cada5dae11297efb36a3
Mostafa Al Moin <sa479374@gmail.com>
2024-10-13 08:38:07 +0600

    solution

--- Basic Maths/M_Reverse_integer.cpp
+++ Basic Maths/M_Reverse_integer.cpp
@@ -3,6 +3,7 @@ https://leetcode.com/problems/reverse-integer/description/
 
 
 // Solution ------>
+// for only positive
 class Solution {
 public:
     int reverse(int x) {
@@ -19,5 +20,26 @@ public:
             x/=10;
         }
         return ans;
+    }
+};
+
+// for positive and negative
+class Solution {
+public:
+    int reverse(int x) {
+        
+        int res = 0;
+        int temp = abs(x);
+
+        while (temp > 0) {
+            int rem = temp % 10;
+
+            if ((res > INT_MAX / 10) || (res < INT_MIN / 10)) return 0;
+
+            res = res * 10 + rem;
+            temp = temp / 10;
+        }
+
+        return x < 0 ? -res : res;
     }
 };
