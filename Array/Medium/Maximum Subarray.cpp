4d96b35434ca85c956f5fd46a83eaac4b92b99ff
Mostafa Al Moin <sa479374@gmail.com>
2024-10-20 08:54:12 +0600

    printing maximum sum subarray

--- Array/Medium/Maximum Subarray.cpp
+++ Array/Medium/Maximum Subarray.cpp
@@ -42,3 +42,38 @@ public:
         return maxSum;
     }
 };
+
+//If You want to print the maximum sum subarray
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int sum = 0;
+        int maxSum = INT_MIN;
+        int startIdx = -1;
+        int endIdx = -1;
+        int ansStartIdx = -1;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (sum == 0) {
+                startIdx = i;
+            }
+            sum += nums[i];
+            
+            if (sum > maxSum) {
+                maxSum = sum;
+                ansStartIdx = startIdx;
+                endIdx = i;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        for (int i = ansStartIdx; i <= endIdx; i++) {
+            cout << nums[i] << " "; // printing maximum sum subarray
+        }
+        return maxSum;
+    }
+};
