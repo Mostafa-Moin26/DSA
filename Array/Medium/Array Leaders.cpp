9e041b07d1d39ed87284156bd7e57eab39d268b4
Mostafa Al Moin <sa479374@gmail.com>
2024-11-02 07:37:29 +0600

    Array Leaders

---
+++ Array/Medium/Array Leaders.cpp
@@ -0,0 +1,73 @@
+// Problem link ---->
+https://www.geeksforgeeks.org/problems/leaders-in-an-array-1587115620/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=leaders-in-an-array
+
+// Solutions
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find the leaders in the array.
+  public:
+    vector<int> leaders(vector<int>& arr) {
+        // Code here
+        vector<int> res;
+        int n = arr.size();
+        int mxx = arr[n - 1];
+        
+        for (int i = n - 1; i >= 0; i--) {
+            
+            if (arr[i] >= mxx) {
+                res.push_back(arr[i]);
+            }
+            mxx = max(mxx, arr[i]);
+        }
+        reverse(res.begin(), res.end());
+        
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
+        Solution obj;
+        vector<int> result = obj.leaders(a);
+
+        // Print the result in the required format
+        if (result.empty()) {
+            cout << "[]"; // Print empty brackets if no leaders are found
+        } else {
+            for (int i = 0; i < result.size(); i++) {
+                if (i != 0)
+                    cout << " ";
+                cout << result[i];
+            }
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
