4a15eb1af2519c6884367b1773c123e5a6eaa253
Mostafa Al Moin <sa479374@gmail.com>
2025-07-18 11:32:27 +0600

    Optimal Solution added

--- Binary Search/BS on Answers/Minimize Max Distance to Gas Station.cpp
+++ Binary Search/BS on Answers/Minimize Max Distance to Gas Station.cpp
@@ -71,3 +71,107 @@ class Solution {
         return round(pq.top().first * 100.0) / 100.0;  
     }
 };
+
+// Optimal using Binary search
+class Solution {
+  public:
+  
+    int numberOfGasSationRequired(double dist, vector<int> &stations) {
+        
+        int cnt = 0;
+        
+        for (int i = 0; i < stations.size() - 1; i++) {
+            
+            int numberInBetween = (stations[i + 1] - stations[i]) / dist;
+            
+            if ((stations[i + 1] - stations[i]) == numberInBetween * dist) {
+                numberInBetween--;
+            }
+            
+            cnt += numberInBetween;
+            
+        }
+        
+        return cnt;
+    }
+    double findSmallestMaxDist(vector<int> &stations, int k) {
+        
+        int n = stations.size();
+        
+        double low = 0, high = 0;
+        
+        for (int i = 0; i < n - 1; i++) {
+            high = max(high, (double)(stations[i + 1] - stations[i]));
+        }
+        
+        double diff = 0.000001;
+        
+        double ans = 0;
+        
+        while (high - low > diff) {
+            
+            double mid = (low + high) / 2.0;
+            
+            int cnt = numberOfGasSationRequired(mid, stations);
+            
+            if (cnt > k) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+            
+        }
+        
+        return high;
+    }
+};
+
+// Another Optimal binary search approach
+class Solution {
+  public:
+  
+    bool check(long double dist, vector<int> &stations, int k) {
+        
+        int cnt = 0;
+        
+        for (int i = 0; i < stations.size() - 1; i++) {
+            
+            int diff = (stations[i + 1] - stations[i]);
+            
+            if (diff > dist) {
+                cnt += diff / dist;
+            }
+        }
+        
+        if (cnt > k) {
+            return false;
+        }
+        
+        return true;
+    }
+    double findSmallestMaxDist(vector<int> &stations, int k) {
+        
+        int n = stations.size();
+        
+        double low = 0, high = stations[n - 1] - stations[0];
+        
+        double diff = 0.000001;
+        
+        double ans = 0;
+        
+        while (low <= high) {
+            
+            double mid = (low + high) / 2;
+            
+            if (check(mid, stations, k)) {
+                ans = mid;
+                high = mid - diff;
+            } else {
+                low = mid + diff;
+            }
+            
+        }
+        
+        return ans;
+    }
+};
