bbfb0c4e115c61b75c01113f695290e49d377747
Mostafa Al Moin <sa479374@gmail.com>
2025-08-02 09:13:19 +0600

    Most optimize solution

--- Basic Maths/E_add_string.cpp
+++ Basic Maths/E_add_string.cpp
@@ -151,3 +151,26 @@ public:
        return res;
     }
 };
+
+// Most Optimized code
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int carry = 0;
+        string ans = "";
+        int i = num1.size() - 1;
+        int j = num2.size() - 1;
+
+        while (i >= 0 || j >= 0 || carry) {
+            if (i >= 0) carry += num1[i--] - '0';
+            if (j >= 0) carry += num2[j--] - '0';
+
+            ans += carry % 10 + '0';
+            carry /= 10;
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
