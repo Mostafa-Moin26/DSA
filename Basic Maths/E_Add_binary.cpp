5e316305ce6599c7c4ac8094ff80978fd6f3dc32
Mostafa Al Moin <sa479374@gmail.com>
2025-08-02 07:26:50 +0600

    Mix Solution

--- Basic Maths/E_Add_binary.cpp
+++ Basic Maths/E_Add_binary.cpp
@@ -78,3 +78,31 @@ public:
         return ans;
     }
 };
+
+// Mix solution
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        
+        if (b.size() > a.size()) {
+          return addBinary(b, a);
+        }
+        reverse(a.begin(), a.end());
+        reverse(b.begin(), b.end());
+        string ans = "";
+        int carry = 0;
+
+        for (int i = 0; i < a.size() || carry; i++) {
+
+            if (i < a.size()) carry += a[i] - '0';
+            if (i < b.size()) carry += b[i] - '0';
+
+            ans += carry % 2 + '0';
+            carry /= 2;
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
