909d4ced05aa577d854710cddbe19a24f8f8744f
Mostafa Al Moin <sa479374@gmail.com>
2025-08-08 19:54:24 +0600

    more solution added

--- Array/Easy/Single Number.cpp
+++ Array/Easy/Single Number.cpp
@@ -67,3 +67,38 @@ public:
         
     }
 };
+
+// Beats 100%
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        if (nums.size() == 1) return nums[0];
+
+        int ans = 0;
+
+        for (int i = 0; i < nums.size(); ++i) ans ^= nums[i];
+
+
+        return ans;
+        
+    }
+};
+
+// sort the array
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+
+        for (int i = 1; i < nums.size(); i += 2) {
+
+            if (nums[i] != nums[i - 1]) {
+                return nums[i - 1];
+            }
+        }
+
+        return nums[nums.size() - 1];
+    }
+};
