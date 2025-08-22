67c622a8aeb5352cbcd33fe37234a945af8c8481
Mostafa Al Moin <sa479374@gmail.com>
2025-08-22 08:32:58 +0600

    one more solution added on Jump Game II

--- Greedy Algorithm/Medium & Hard/Jump Game II.cpp
+++ Greedy Algorithm/Medium & Hard/Jump Game II.cpp
@@ -49,3 +49,27 @@ public:
 
     }
 };
+
+// code from striver
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+
+        int n = nums.size();
+        int jump = 0, left = 0, right = 0;
+
+        while (right < n - 1) {
+            int farthest = 0;
+
+            for (int i = left; i <= right; i++) {
+                farthest = max(farthest, i + nums[i]);
+            }
+            jump++;
+            left = right + 1;
+            right = farthest;
+        }
+
+        return jump;
+
+    }
+};
