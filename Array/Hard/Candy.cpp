b8d55bb117360418132dcc95ced5c905aa9ddc84
Mostafa Al Moin <sa479374@gmail.com>
2026-04-26 11:52:10 +0600

    Candy

---
+++ Array/Hard/Candy.cpp
@@ -0,0 +1,29 @@
+// Problem link ---->
+https://leetcode.com/problems/candy/description/
+
+// Solution ---->
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+
+        int n = ratings.size();
+        
+        vector<int> candies(n, 1);
+
+        // Left pass
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] += candies[i - 1];
+            }
+        }
+
+        // Right pass
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        return accumulate(candies.begin(), candies.end(), 0);
+    }
+};
